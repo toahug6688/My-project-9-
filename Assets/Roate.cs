7d6b62a64571a4a902a23248501a51c82cebf538
ToAHuG <c121776235@gmail.com>
2024-10-31 00:03:05 +0800

    撰寫程式
    設計讓圓盤旋轉

---
+++ Assets/Roate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Rotate : MonoBehaviour
+{
+    float rotSpeed = 0; // 旋轉速度
+    void Start()
+    {
+    }
+    void Update()
+    {
+        // 若點擊滑鼠就要設定旋轉速度
+        if (Input.GetMouseButtonDown(0))
+        {
+            rotSpeed = 10;
+        }
+        // 旋轉速度、讓輪盤旋轉
+        transform.Rotate(0, 0, rotSpeed);
+    }
+}
